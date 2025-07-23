2e879bbf9c3d29c9a9d472e986e1f9f612a64c7f
Yuzan <yuzan.ymmt@gmail.com>
2025-07-23 19:50:48 +0900

    くるくる表示追加

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,14 +1,21 @@
-import { Loader2 } from "lucide-react"
-import { cn } from "@/lib/utils"
+import type React from "react"
+import { cn } from "@/lib/utils" // shadcn/uiのutilsを使用する場合
 
-interface LoaderProps {
-  className?: string;
-}
+interface LoaderProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export function Loader({ className }: LoaderProps) {
+export function Loader({ className, ...props }: LoaderProps) {
   return (
-    <Loader2 
-      className={cn("animate-spin text-spotify-green", className)} 
-    />
+    <div
+      className={cn(
+        "h-5 w-5 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]",
+        className
+      )}
+      role="status"
+      {...props}
+    >
+      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+        Loading...
+      </span>
+    </div>
   )
 }
